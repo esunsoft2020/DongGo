5d0f3a8bcd20efe7b70409a0e9883466c0efcf13
esunsoft2020 <esunsoft2020@gmail.com>
2021-02-08 10:29:53 +0900

    add IntroActivity

---
+++ app/src/main/java/com/esunsoft2020/donggo/IntroActivity.java
@@ -0,0 +1,14 @@
+package com.esunsoft2020.donggo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+    }
+}
